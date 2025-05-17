a56d27c8b51de2f9aafcdef2fcbb1becbd040a66
Douglas <douglas.jesus077@academico.ifs.edu.br>
2025-05-17 14:41:12 -0300

    refactor(jwt): improve token generation and verification functions for readability

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -6,5 +6,17 @@ if (!secret) {
   throw new Error('Secret key for JWT is not defined');
 }
 
-export const generateToken = (user) => sign({ id: user.id, type: user.user_type }, secret);
-export const verifyToken = (token) => verify(token, secret);
+export const generateToken = (user) => {
+  return sign(
+    {
+      id: user.id,
+      type: user.user_type,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60
+    },
+    secret
+  );
+};
+
+export const verifyToken = (token) => {
+  return verify(token, secret);
+};
