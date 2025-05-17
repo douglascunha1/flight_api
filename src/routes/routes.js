8a40f9353f537d07f75493dab8e2b4a1c5a487a5
Douglas <douglas.jesus077@academico.ifs.edu.br>
2025-05-16 23:02:43 -0300

    feat(routes): add CRUD operations for passengers, flights, aircrafts, boarding passes, and users chore(package): update devDependencies to include eslint and globals

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,22 +1,71 @@
-import { getPassengers, getPassengerById } from "../handlers/passengerHandler.js"
-import { getFlight, getFlightById } from "../handlers/flightHandler.js"
-import { getAircraft, getAircraftById } from "../handlers/aircraftHandler.js"
-import { getBoardingPass, getBoardingPassById } from "../handlers/boardingPassHandler.js"
-import { getUser, getUserById } from "../handlers/userHandler.js"
+import {
+  getPassengers,
+  getPassengerById,
+  createPassenger,
+  updatePassenger,
+  deletePassenger
+} from "../handlers/passengerHandler.js";
+
+import {
+  getFlight,
+  getFlightById,
+  createFlight,
+  updateFlight,
+  deleteFlight
+} from "../handlers/flightHandler.js";
+
+import {
+  getAircraft,
+  getAircraftById,
+  createAircraft,
+  updateAircraft,
+  deleteAircraft
+} from "../handlers/aircraftHandler.js";
+
+import {
+  getBoardingPass,
+  getBoardingPassById,
+  createBoardingPass,
+  updateBoardingPass,
+  deleteBoardingPass
+} from "../handlers/boardingPassHandler.js";
+
+import {
+  getUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} from "../handlers/userHandler.js";
 
 export const routes = (app) => {
-    app.get('/passengers', getPassengers);
-    app.get('/passengers/:id', getPassengerById);
+  app.get('/passengers', getPassengers);
+  app.get('/passengers/:id', getPassengerById);
+  app.post('/passengers', createPassenger);
+  app.put('/passengers/:id', updatePassenger);
+  app.delete('/passengers/:id', deletePassenger);
 
-    app.get('/flights', getFlight);
-    app.get('/flights/:id', getFlightById);
+  app.get('/flights', getFlight);
+  app.get('/flights/:id', getFlightById);
+  app.post('/flights', createFlight);
+  app.put('/flights/:id', updateFlight);
+  app.delete('/flights/:id', deleteFlight);
 
-    app.get('/aircrafts', getAircraft);
-    app.get('/aircrafts/:id', getAircraftById);
+  app.get('/aircrafts', getAircraft);
+  app.get('/aircrafts/:id', getAircraftById);
+  app.post('/aircrafts', createAircraft);
+  app.put('/aircrafts/:id', updateAircraft);
+  app.delete('/aircrafts/:id', deleteAircraft);
 
-    app.get('/boarding-passes', getBoardingPass);
-    app.get('/boarding-passes/:id', getBoardingPassById);
+  app.get('/boarding-passes', getBoardingPass);
+  app.get('/boarding-passes/:id', getBoardingPassById);
+  app.post('/boarding-passes', createBoardingPass);
+  app.put('/boarding-passes/:id', updateBoardingPass);
+  app.delete('/boarding-passes/:id', deleteBoardingPass);
 
-    app.get('/users', getUser);
-    app.get('/users/:id', getUserById);
-}
+  app.get('/users', getUser);
+  app.get('/users/:id', getUserById);
+  app.post('/users', createUser);
+  app.put('/users/:id', updateUser);
+  app.delete('/users/:id', deleteUser);
+};
